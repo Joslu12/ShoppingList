3bc0f1abe393069941258ec678e0335de5d396b1
Joslu12 <joslu1299@gmail.com>
2020-12-12 13:50:22 +0100

    Implementacion de StockActivity y StockListActivity

--- app/src/main/java/com/example/shoppinglist/MainActivity.java
+++ app/src/main/java/com/example/shoppinglist/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 import com.example.shoppinglist.shoppinglists_activities.ShoppingListActivity;
 import com.example.shoppinglist.shoppinglists_activities.ShoppingListListActivity;
+import com.example.shoppinglist.stocks_activities.StockListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -29,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
                 break;
 
             case R.id.btnMainStocks:
-                // TODO:
+                startActivity(new Intent(getApplicationContext(), StockListActivity.class));
                 break;
         }
     }
---
+++ app/src/main/java/com/example/shoppinglist/stocks_activities/StockActivity.java
@@ -0,0 +1,118 @@
+package com.example.shoppinglist.stocks_activities;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.DialogInterface;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.shoppinglist.R;
+
+import bd.BaseDatosUtils;
+import bd.dao.ShoppingListDao;
+import bd.dao.StockDao;
+import model.ShoppingList;
+import model.Stock;
+
+public class StockActivity extends AppCompatActivity {
+
+    //---- Constantes y Definiciones ----
+
+    //---- Atributos ----
+    private SQLiteDatabase bd;
+    private Stock stock;
+
+    //---- Elementos de la Vista ----
+
+    //---- Metodos ----
+    private void loadStock(int id) {
+        // Cargamos la lista de la compra almacenada en la BD con el id especificado
+        StockDao daoSL = new StockDao(bd);
+        stock = daoSL.findById(id);
+    }
+
+    private void deleteDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(R.string.warnig_dialog);
+        builder.setIcon(android.R.drawable.ic_dialog_alert);
+        builder.setNegativeButton(R.string.delete, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.dismiss();
+                deleteStock();
+            }
+        });
+        builder.setNeutralButton(R.string.cancel, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int i) {
+                dialog.dismiss();
+            }
+        });
+        builder.show();
+    }
+
+    private void deleteStock(){
+        // Guardamos el nombre para el mensage mostrado despues
+        String name = stock.getName();
+
+        // Eliminamos de la BD el inventario
+        StockDao dao = new StockDao(bd);
+        dao.remove(stock);
+
+        // Finalizamos la actividad
+        this.finish();
+
+        // Mostramos un mensaje informativo de la accion realizada
+        String msg = String.format(getResources().getString(R.string.info_msg_stock_deleted), name);
+        Toast.makeText(getApplicationContext(),msg,Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stock);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Obtenemos una conexion a la BD con el SingletonMap
+        bd = BaseDatosUtils.getWritableDatabaseConnection(getApplicationContext());
+
+        // Cargamos de la base de datos la lista de la compra
+        loadStock(getIntent().getIntExtra("ID",-1));
+
+        // Configuramos lo necesario de la barra de herramientas
+        getSupportActionBar().setTitle(stock.getName());
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_products_list ,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        boolean ok = true;
+
+        switch (item.getItemId()) {
+            case R.id.delete:
+                deleteDialog();
+                break;
+
+            case R.id.action_settings:
+                break;
+
+            default:
+                ok = super.onOptionsItemSelected(item);
+                break;
+        }
+        return ok;
+    }
+}
